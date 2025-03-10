3b590702cffe75bdd7d48673daccf3916a413f7e
hau2 <leconghau095@gmail.com>
2025-03-10 10:18:18 +0700

    Add nhan su cap cao

--- src/app/[locale]/tong-quan/page.tsx
+++ src/app/[locale]/tong-quan/page.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function TongQuan() {
   return (
@@ -116,6 +117,215 @@ export default function TongQuan() {
               height={500}
               className="rounded-lg shadow-lg"
             />
+          </div>
+        </div>
+      </div>
+      <div className="pl-[32px] pr-[32px] my-[32px]">
+        <h2 className="text-black/80 font-semibold text-[32px]">
+          Nhân sự cấp cao
+        </h2>
+        <div className="my-[16px] flex flex-row items-center justify-center gap-[32px]">
+          <div className="flex flex-col items-center">
+            <div className="w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+              <Image
+                src={"/images/nhansu1.png"}
+                width={200}
+                height={200}
+                alt="nhansu"
+              />
+            </div>
+            <h5 className="font-semibold text-[16px] text-orange-800/70">
+              Trần Quang Tuấn
+            </h5>
+            <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+              Giám đốc điều hành
+            </p>
+            <Link
+              className="text-[14px] text-blue-800 italic font-semibold"
+              href={"/"}
+            >
+              Xem thêm
+            </Link>
+          </div>
+        </div>
+        <div className="my-[16px] flex flex-row items-center justify-center gap-[32px]">
+          <div className="flex flex-col items-center">
+            <div className="w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+              <Image
+                src={"/images/nhansu1.png"}
+                width={200}
+                height={200}
+                alt="nhansu"
+              />
+            </div>
+            <h5 className="font-semibold text-[16px] text-orange-800/70">
+              Trần Quang Tuấn
+            </h5>
+            <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+              Giám đốc điều hành
+            </p>
+            <Link
+              className="text-[14px] text-blue-800 italic font-semibold"
+              href={"/"}
+            >
+              Xem thêm
+            </Link>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+              <Image
+                src={"/images/nhansu1.png"}
+                width={200}
+                height={200}
+                alt="nhansu"
+              />
+            </div>
+            <h5 className="font-semibold text-[16px] text-orange-800/70">
+              Trần Quang Tuấn
+            </h5>
+            <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+              Giám đốc điều hành
+            </p>
+            <Link
+              className="text-[14px] text-blue-800 italic font-semibold"
+              href={"/"}
+            >
+              Xem thêm
+            </Link>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+              <Image
+                src={"/images/nhansu1.png"}
+                width={200}
+                height={200}
+                alt="nhansu"
+              />
+            </div>
+            <h5 className="font-semibold text-[16px] text-orange-800/70">
+              Trần Quang Tuấn
+            </h5>
+            <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+              Giám đốc điều hành
+            </p>
+            <Link
+              className="text-[14px] text-blue-800 italic font-semibold"
+              href={"/"}
+            >
+              Xem thêm
+            </Link>
+          </div>
+        </div>
+        <div className="my-[16px] flex flex-row items-center justify-center gap-[32px]">
+          <div className="flex flex-col items-center">
+            <div className="w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+              <Image
+                src={"/images/nhansu1.png"}
+                width={200}
+                height={200}
+                alt="nhansu"
+              />
+            </div>
+            <h5 className="font-semibold text-[16px] text-orange-800/70">
+              Trần Quang Tuấn
+            </h5>
+            <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+              Giám đốc điều hành
+            </p>
+            <Link
+              className="text-[14px] text-blue-800 italic font-semibold"
+              href={"/"}
+            >
+              Xem thêm
+            </Link>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+              <Image
+                src={"/images/nhansu1.png"}
+                width={200}
+                height={200}
+                alt="nhansu"
+              />
+            </div>
+            <h5 className="font-semibold text-[16px] text-orange-800/70">
+              Trần Quang Tuấn
+            </h5>
+            <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+              Giám đốc điều hành
+            </p>
+            <Link
+              className="text-[14px] text-blue-800 italic font-semibold"
+              href={"/"}
+            >
+              Xem thêm
+            </Link>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+              <Image
+                src={"/images/nhansu1.png"}
+                width={200}
+                height={200}
+                alt="nhansu"
+              />
+            </div>
+            <h5 className="font-semibold text-[16px] text-orange-800/70">
+              Trần Quang Tuấn
+            </h5>
+            <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+              Giám đốc điều hành
+            </p>
+            <Link
+              className="text-[14px] text-blue-800 italic font-semibold"
+              href={"/"}
+            >
+              Xem thêm
+            </Link>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+              <Image
+                src={"/images/nhansu1.png"}
+                width={200}
+                height={200}
+                alt="nhansu"
+              />
+            </div>
+            <h5 className="font-semibold text-[16px] text-orange-800/70">
+              Trần Quang Tuấn
+            </h5>
+            <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+              Giám đốc điều hành
+            </p>
+            <Link
+              className="text-[14px] text-blue-800 italic font-semibold"
+              href={"/"}
+            >
+              Xem thêm
+            </Link>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+              <Image
+                src={"/images/nhansu1.png"}
+                width={200}
+                height={200}
+                alt="nhansu"
+              />
+            </div>
+            <h5 className="font-semibold text-[16px] text-orange-800/70">
+              Trần Quang Tuấn
+            </h5>
+            <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+              Giám đốc điều hành
+            </p>
+            <Link
+              className="text-[14px] text-blue-800 italic font-semibold"
+              href={"/"}
+            >
+              Xem thêm
+            </Link>
           </div>
         </div>
       </div>
