737a7f9a6772971933ac9090a9b80f69c4190832
hau2 <leconghau095@gmail.com>
2025-03-18 00:09:43 +0700

    Update Nhan Su Cap Cao

--- src/app/[locale]/about/staff/page.tsx
+++ src/app/[locale]/about/staff/page.tsx
@@ -4,149 +4,192 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Staff() {
-    return (
-      <div className="p-[32px]">
-          <h2 className="text-black/80 font-semibold text-[32px]">
-              Nhân sự cấp cao
-          </h2>
-          <div className="my-[16px] flex flex-row items-center justify-center gap-[32px]">
-              <div className="flex flex-col items-center">
-                  <div className="w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
-                      <Image
-                          src={"/images/nhansu1.png"}
-                          width={200}
-                          height={200}
-                          alt="nhansu"
-                      />
-                  </div>
-                  <h5 className="font-semibold text-[16px] text-orange-800/70">
-                      Trần Quang Tuấn
-                  </h5>
-                  <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
-                      Giám đốc điều hành
-                  </p>
-                  <Link
-                      className="text-[14px] text-blue-800 italic font-semibold"
-                      href={"/"}
-                  >
-                      Xem thêm
-                  </Link>
-              </div>
+  return (
+    <div className="p-[32px]">
+      <h2 className="text-black/80 font-semibold text-[32px]">
+        Nhân sự cấp cao
+      </h2>
+      <div className="my-[64px] flex flex-row items-center justify-center gap-[32px]">
+        <div className="flex flex-col items-center">
+          <div className="relative w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+            <Image
+              src={"/images/sdtc1.png"}
+              layout="fill"
+              objectFit="cover"
+              alt="Nguyễn Thanh Bình"
+            />
           </div>
-          <div className="my-[16px] flex flex-row items-center justify-center gap-[64px]">
-              <div className="flex flex-col items-center">
-                  <div className="w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
-                      <Image
-                          src={"/images/nhansu1.png"}
-                          width={200}
-                          height={200}
-                          alt="nhansu"
-                      />
-                  </div>
-                  <h5 className="font-semibold text-[16px] text-orange-800/70">
-                      Trần Quang Tuấn
-                  </h5>
-                  <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
-                      Giám đốc điều hành
-                  </p>
-                  <Link
-                      className="text-[14px] text-blue-800 italic font-semibold"
-                      href={"/"}
-                  >
-                      Xem thêm
-                  </Link>
-              </div>
-              <div className="flex flex-col items-center">
-                  <div className="w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
-                      <Image
-                          src={"/images/nhansu1.png"}
-                          width={200}
-                          height={200}
-                          alt="nhansu"
-                      />
-                  </div>
-                  <h5 className="font-semibold text-[16px] text-orange-800/70">
-                      Trần Quang Tuấn
-                  </h5>
-                  <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
-                      Giám đốc điều hành
-                  </p>
-                  <Link
-                      className="text-[14px] text-blue-800 italic font-semibold"
-                      href={"/"}
-                  >
-                      Xem thêm
-                  </Link>
-              </div>
-          </div>
-          <div className="my-[16px] flex flex-row items-center justify-center gap-[64px]">
-              <div className="flex flex-col items-center">
-                  <div className="w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
-                      <Image
-                          src={"/images/nhansu1.png"}
-                          width={200}
-                          height={200}
-                          alt="nhansu"
-                      />
-                  </div>
-                  <h5 className="font-semibold text-[16px] text-orange-800/70">
-                      Trần Quang Tuấn
-                  </h5>
-                  <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
-                      Giám đốc điều hành
-                  </p>
-                  <Link
-                      className="text-[14px] text-blue-800 italic font-semibold"
-                      href={"/"}
-                  >
-                      Xem thêm
-                  </Link>
-              </div>
-              <div className="flex flex-col items-center">
-                  <div className="w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
-                      <Image
-                          src={"/images/nhansu1.png"}
-                          width={200}
-                          height={200}
-                          alt="nhansu"
-                      />
-                  </div>
-                  <h5 className="font-semibold text-[16px] text-orange-800/70">
-                      Trần Quang Tuấn
-                  </h5>
-                  <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
-                      Giám đốc điều hành
-                  </p>
-                  <Link
-                      className="text-[14px] text-blue-800 italic font-semibold"
-                      href={"/"}
-                  >
-                      Xem thêm
-                  </Link>
-              </div>
-              <div className="flex flex-col items-center">
-                  <div className="w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
-                      <Image
-                          src={"/images/nhansu1.png"}
-                          width={200}
-                          height={200}
-                          alt="nhansu"
-                      />
-                  </div>
-                  <h5 className="font-semibold text-[16px] text-orange-800/70">
-                      Trần Quang Tuấn
-                  </h5>
-                  <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
-                      Giám đốc điều hành
-                  </p>
-                  <Link
-                      className="text-[14px] text-blue-800 italic font-semibold"
-                      href={"/"}
-                  >
-                      Xem thêm
-                  </Link>
-              </div>
-          </div>
+          <h5 className="font-semibold text-[16px] text-orange-800/70">
+            Nguyễn Thanh Bình
+          </h5>
+          <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+            Tổng giám đốc
+          </p>
+        </div>
       </div>
-    );
+
+      <div className="my-[64px] flex flex-row items-center justify-center gap-[64px]">
+        <div className="flex flex-col items-center">
+          <div className="relative w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+            <Image
+              src={"/images/sdtc-tranngocchinh.png"}
+              layout="fill"
+              objectFit="cover"
+              alt="Trần Ngọc Chính"
+            />
+          </div>
+          <h5 className="font-semibold text-[16px] text-orange-800/70">
+            Trần Ngọc Chính
+          </h5>
+          <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+            Phó tổng giám đốc - Trưởng phòng thiết kế
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div className="relative w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+            <Image
+              src={"/images/sdtc-nguyenthikimoanh.png"}
+              layout="fill"
+              objectFit="cover"
+              alt="Nguyễn Thị Kim Oanh"
+            />
+          </div>
+          <h5 className="font-semibold text-[16px] text-orange-800/70">
+            Nguyễn Thị Kim Oanh
+          </h5>
+          <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+            Giám đốc tài chính
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div className="relative w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+            <Image
+              src={"/images/sdtc-thonglamgieng.png"}
+              layout="fill"
+              objectFit="cover"
+              alt="Nguyễn Thị Kim Oanh"
+            />
+          </div>
+          <h5 className="font-semibold text-[16px] text-orange-800/70">
+            Thòng Lằm Giểng
+          </h5>
+          <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+            Trợ lý giám đốc - Giám đốc kinh doanh
+          </p>
+        </div>
+
+      </div>
+
+      <div className="my-[64px] flex flex-row items-center justify-center gap-[64px]">
+        <div className="flex flex-col items-center">
+          <div className="relative w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+            <Image
+              src={"/images/sdtc-dangthituyetnhung.png"}
+              layout="fill"
+              objectFit="cover"
+              alt="Đặng Thị Tuyết Nhung"
+            />
+          </div>
+          <h5 className="font-semibold text-[16px] text-orange-800/70">
+            Đặng Thị Tuyết Nhung
+          </h5>
+          <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+            Trưởng phòng kế toán
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div className="relative w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+            <Image
+              src={"/images/sdtc-maithibachdan.png"}
+              layout="fill"
+              objectFit="cover"
+              alt="Mai Thị Bạch Đàn"
+            />
+          </div>
+          <h5 className="font-semibold text-[16px] text-orange-800/70">
+            Mai Thị Bạch Đàn
+          </h5>
+          <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+            Trưởng phòng vật tư
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div className="relative w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+            <Image
+              src={"/images/sdtc-truongthanhhai.png"}
+              layout="fill"
+              objectFit="cover"
+              alt="Trương Thanh Hải"
+            />
+          </div>
+          <h5 className="font-semibold text-[16px] text-orange-800/70">
+            Trương Thanh Hải
+          </h5>
+          <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+            Trưởng phòng kỹ thuật
+          </p>
+        </div>
+
+      </div>
+
+      <div className="my-[64px] flex flex-row items-center justify-center gap-[64px]">
+        <div className="flex flex-col items-center">
+          <div className="relative w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+            <Image
+              src={"/images/sdtc-honguyenkhang.png"}
+              layout="fill"
+              objectFit="cover"
+              alt="Hồ Nguyên Khang"
+            />
+          </div>
+          <h5 className="font-semibold text-[16px] text-orange-800/70">
+            Hồ Nguyên Khang
+          </h5>
+          <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+            Chỉ huy trưởng
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div className="relative w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+            <Image
+              src={"/images/sdtc-trandanghoi.png"}
+              layout="fill"
+              objectFit="cover"
+              alt="Trần Đăng Hồi"
+            />
+          </div>
+          <h5 className="font-semibold text-[16px] text-orange-800/70">
+            Trần Đăng Hồi
+          </h5>
+          <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+            Chỉ huy phó công trình
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div className="relative w-[200px] h-[200px] rounded-[50%] bg-amber-600 overflow-hidden border-8 shadow-2xl">
+            <Image
+              src={"/images/sdtc-lexuanluong.png"}
+              layout="fill"
+              objectFit="cover"
+              alt="Lê Xuân Lương"
+            />
+          </div>
+          <h5 className="font-semibold text-[16px] text-orange-800/70">
+            Lê Xuân Lương
+          </h5>
+          <p className="font-semibold text-black/60 text-[14px] mt-[8px]">
+            Phó phòng thiết kế
+          </p>
+        </div>
+
+      </div>
+    </div>
+
+  );
 }
