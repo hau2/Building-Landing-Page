e65eb8a73d77ecb5bf17326c30b8d29452f5215f
hau2 <leconghau095@gmail.com>
2025-06-06 13:22:35 +0700

    Add sitemap & robot.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl: 'https://binhdoanhcons.vn',  // Thay bằng tên miền thật của bạn
+  generateRobotsTxt: true,              // Tự động tạo robots.txt
+  sitemapSize: 7000,                    // Mỗi sitemap chứa tối đa 7000 URL
+  changefreq: 'daily',
+  priority: 0.7,
+};
